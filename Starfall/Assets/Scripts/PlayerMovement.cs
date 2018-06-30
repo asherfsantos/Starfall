730b6beedeb0c88f8e274790dfa26640a9b1072b
asherfsantos <40329281+asherfsantos@users.noreply.github.com>
2018-06-30 00:55:40 -0700

    Stars move

---
+++ Starfall/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public Rigidbody2D player;
+	public GameObject currentStar;
+	private bool onStar = true;
+	// Use this for initialization
+	void Start () 
+	{
+		//player = FindObjec
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		MoveWithStar();
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+
+		if(other.tag == "Star")
+			currentStar = other.gameObject;
+			transform.position = other.transform.position;
+			onStar = true;
+	}
+
+	void MoveWithStar()
+	{
+		if(onStar)
+		{
+			transform.position = currentStar.transform.position;
+		}
+	}
+}
---
+++ Starfall/Assets/Scripts/StarMovements.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StarMovements : MonoBehaviour {
+
+	public float movementSpeed = 0.5f;
+	public float startPosition;
+	public float endPosition;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		moveStar();
+		
+	}
+
+	void moveStar()
+	{
+            float step = movementSpeed * Time.deltaTime;
+            transform.position = Vector3.MoveTowards(transform.position, Camera.main.ViewportToWorldPoint(new Vector3(0,1,0)), step);
+	}
+}

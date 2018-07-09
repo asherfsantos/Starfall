5de6311fb1e640639472b262f14741660e1e1b02
asherfsantos <40329281+asherfsantos@users.noreply.github.com>
2018-07-08 22:37:41 -0700

    level 2 polish

--- Starfall/Assets/Scripts/StarSpawns.cs
+++ Starfall/Assets/Scripts/StarSpawns.cs
@@ -7,9 +7,9 @@ public class StarSpawns : MonoBehaviour
 	public GameObject star;
 	public float nextSpawn = 0.0f;
 	public float spawnRate = 1.0f;
-	public float spawnLocationX = 20.0f;
-	public float lowerY = -5.0f;
-	public float upperY = -3.0f;
+	public float spawnLocationX = 81.0f;
+	public float upperY = -6.0f;
+	public float lowerY = -8.0f;
 	public float randomY;
 	public Vector2 spawnLocation;
 

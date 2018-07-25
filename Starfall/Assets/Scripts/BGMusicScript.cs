e02f1ea44d8bfe1a790d42c28a32d3d660d3c71d
asherfsantos <40329281+asherfsantos@users.noreply.github.com>
2018-07-25 04:21:54 -0700

    level 1 audio fix

---
+++ Starfall/Assets/Scripts/BGMusicScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGMusicScript : MonoBehaviour 
+{
+	public AudioSource intro;
+    public AudioSource loop;
+	public AudioSource audioPlayer;
+
+	// Use this for initialization
+	void Start () 
+	{
+		audioPlayer = GetComponent<AudioSource>();
+		audioPlayer = intro;
+        audioPlayer.Play();
+		audioPlayer = loop;
+        audioPlayer.PlayDelayed(intro.clip.length);
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () 
+	{
+
+	}
+}

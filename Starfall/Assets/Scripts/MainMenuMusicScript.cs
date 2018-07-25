3bb61d78bf015f705374a972b9eb47fc98e5d218
asherfsantos <40329281+asherfsantos@users.noreply.github.com>
2018-07-25 05:10:55 -0700

    added rest of main menu music

---
+++ Starfall/Assets/Scripts/MainMenuMusicScript.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuMusicScript : MonoBehaviour 
+{
+	public AudioSource clip1;
+    public AudioSource clip2;
+	public AudioSource clip3;
+	public AudioSource audioPlayer;
+	public bool musicDone;
+
+	// Use this for initialization
+	void Start () 
+	{
+		PlayBGMusic();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () 
+	{
+		if(musicDone)
+		{
+			PlayBGMusic();
+		}
+	}
+
+	void PlayBGMusic()
+	{
+		musicDone = false;
+		audioPlayer = GetComponent<AudioSource>();
+		audioPlayer = clip1;
+        audioPlayer.Play();
+		audioPlayer = clip2;
+        audioPlayer.PlayDelayed(clip1.clip.length);
+		audioPlayer = clip3;
+        audioPlayer.PlayDelayed(clip1.clip.length + clip2.clip.length);
+		Invoke("ResetBool", clip1.clip.length + clip2.clip.length + clip3.clip.length);
+	}
+
+	void ResetBool()
+	{
+		musicDone = true;	
+		print(musicDone);
+	}
+}

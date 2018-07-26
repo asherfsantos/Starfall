822872f5d6bc96c8bb6647768c86b6e373833288
asherfsantos <40329281+asherfsantos@users.noreply.github.com>
2018-07-25 19:02:43 -0700

    Tutorial

---
+++ Starfall/Assets/Scripts/TriggeredUI.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TriggeredUI : MonoBehaviour 
+{
+	public GameObject uiObject;
+
+	void Start()
+	{
+		uiObject.SetActive(false);
+	}
+
+	void OnTriggerEnter2D (Collider2D player)
+	{
+		print("Collision");
+		if (player.gameObject.tag == "Player")
+		{
+			uiObject.SetActive(true);
+		}
+	}
+
+	void OnTriggerExit2D (Collider2D player)
+	{
+		if (player.gameObject.tag == "Player")
+		{
+			uiObject.SetActive(false);
+		}
+	}
+}

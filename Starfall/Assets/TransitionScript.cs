e0d3ac0801415aeffea395e0c5f14d53c9d7b24f
asherfsantos <40329281+asherfsantos@users.noreply.github.com>
2018-07-28 22:21:07 -0700

    transitions

---
+++ Starfall/Assets/TransitionScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TransitionScript : MonoBehaviour 
+{
+
+	// Use this for initialization
+	void Start () 
+	{
+		Invoke("NextScene", 12f);
+	}
+	
+	void NextScene()
+	{
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex +1);
+	}
+}
